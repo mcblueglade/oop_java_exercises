7f3aa1ffc11010abde516464ba40212e74471f09
cublyss <cublyss@gmail.com>
2020-09-09 18:15:31 +0100

    Fix abstract method declarations for abstract classes

--- src/main/java/com/techreturners/cats/Cat.java
+++ src/main/java/com/techreturners/cats/Cat.java
@@ -2,22 +2,15 @@ package com.techreturners.cats;
 
 public abstract class Cat {
 
-    public Boolean isAsleep();// {
-     //   return True;
-    
-    //}
-    public String getSetting();// {
-        // return "";
-    
-    //}  
-    public int getAverageHeight();// {
-        //return 0;
-    //}  
-    public String eat();// {
-        //return "";
-    
-    //}  
-    public String goToSleep();
-    
-    public String wakeUp();
+    public abstract Boolean isAsleep();
+
+    public abstract String getSetting();
+
+    public abstract int getAverageHeight();
+
+    public abstract String eat();
+
+    public abstract String goToSleep();
+
+    public abstract String wakeUp();
 }
